4ec9a516b3eb272c0b6ad5bd0e7c57da78056fda
Jon Peterson <github@joequincy.dev>
2022-08-31 17:28:01 -0600

    add firstrun info
    Signed-off-by: Jon Peterson <github@joequincy.dev>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # why-did-i-block
 A simple site to keep notes about the reasons you blocked someone who absolutely doesn't deserve being committed to memory.
+
+### Getting Started
+
+This project provides a `firstrun.sh` script which can be used to get everything ready:
+```sh
+./scripts/firstrun.sh
+```
+
+Alternatively, make sure you have installed
+- [`nvm`](https://github.com/nvm-sh/nvm#git-install)
+- the correct `node` version (run `nvm use`)
+- [`docker`](https://docs.docker.com/engine/install/) (see [special instructions](https://docs.docker.com/desktop/windows/wsl/) for developing under WSL)
+- [`yarn`](https://classic.yarnpkg.com/lang/en/docs/install/)
---
+++ scripts/firstrun.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if ! [ -d "${HOME}/.nvm/.git" ]; then
+  echo >&2 "nvm is required for this project, please install from"
+  echo >&2 "https://github.com/nvm-sh/nvm#git-install"
+  echo >&2 "then run this firstrun script again"
+  exit 1
+fi
+
+echo "Ensuring the proper Node version is set"
+nvm use
+
+if ! command -v docker >/dev/null 2>&1; then
+  if ! [ -z "$WSL_DISTRO_NAME" ]; then
+    echo >&2 "docker is required for this project, but must be installed"
+    echo >&2 "in the Windows host OS. Please visit this URL for instructions"
+    echo >&2 "https://docs.docker.com/desktop/windows/wsl/"
+    echo >&2 "then run this firstrun script again once docker is installed"
+    exit 2
+  fi
+
+  if ! command -v brew >/dev/null 2>&1; then
+    echo >&2 "docker is required for this project, but this script can only"
+    echo >&2 "auto-install it using homebrew, which was not found."
+    echo >&2 "This script will not attempt to auto-install homebrew."
+    echo >&2 ""
+    echo >&2 "Please either install docker following instructions at"
+    echo >&2 "https://docs.docker.com/engine/install/"
+    echo >&2 ""
+    echo >&2 "Or install homebrew following instructions at"
+    echo >&2 "https://brew.sh/"
+    echo >&2 ""
+    echo >&2 "then run this firstrun script again once docker or homebrew is installed"
+    exit 3
+  fi
+
+  echo "Installing docker..."
+  brew install docker
+fi
+
+if ! command -v yarn >/dev/null 2>&1; then
+  echo "Installing yarn..."
+  npm i -g yarn
+fi
+
+echo "Installing project dependencies with yarn..."
+yarn
